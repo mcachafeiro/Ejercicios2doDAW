14ad0eae61bbf6242e1518316c6d739f9ebb3acd
Marcos Cachafeiro <145364807+mcachafeiro@users.noreply.github.com>
2023-12-14 11:59:27 +0100

    Create datos.js

---
+++ JavaScript/Repaso1eraEv/PracticaExamen/datos.js
@@ -0,0 +1,30 @@
+export const datos = `
+QuotaAmount,StartDate,OwnerName,Username
+    1000,2016-01-01,Chris Riley,trailhead9.ub20k5i9t8ou@example.com
+1530000,2016-02-01,Chris Riley,trailhead9.ub20k5i9t8ou@example.com,,
+, ,
+150000  ,2020-03-01,Chris Riley,trailhead9.ub20k5i9t8ou@example.com
+1646000,2016-01-01,Harold Campbell,trailhead14.jibpbwvuy67t@example.com
+150020,2016-02-01,Harold Campbell,trailhead14.jibpbwvuy67t@example.com
+150000,2012-03-01,Harold Campbell,trailhead14.jibpbwvuy67t@example.com
+150230,2016-01-01,Jessica Nichols,trailhead19.d1fxj2goytkp@example.com
+150000,2000-02-01,Jessica Nichols,trailhead19.d1fxj2goytkp@example.com
+
+
+150900,2016-03-01,Jessica Nichols,trailhead19.d1fxj2goytkp@example.com
+150090,2016-01-01,Catherine Brown,trailhead16.kojyepokybge@example.com
+   150000,2016-02-01,Catherine Brown,trailhead16.kojyepokybge@example.com
+150000,2016-03-01,  Catherine Brown,trailhead16.kojyepokybge@example.com  
+100000,2018-01-01,Kelly Frazier,trailhead7.zdcsy4ax10mr@example.com
+14370000,2016-02-01,Kelly Frazier,  trailhead7.zdcsy4ax10mr@example.com
+150000,2016-03-01,Kelly Frazier,trailhead7.zdcsy4ax10mr@example.com
+650000,2015-01-01,Dennis Howard,trailhead4.wfokpckfroxp@example.com
+ , ,
+
+
+ , 
+
+1750000,2016-02-01,Dennis Howard,trailhead4.wfokpckfroxp@example.com
+     150000,2016-03-01,    Dennis Howard,trailhead4.wfokpckfroxp@example.com      
+150000,2000-03-01,Dennis Howard,trailhead4.wfokpckfroxp@example.com
+`;
